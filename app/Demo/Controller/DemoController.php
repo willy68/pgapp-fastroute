7a325b90db5af3fc29c5673de518f5a3a079e6e1
willy68 <william.lety@gmail.com>
2021-02-21 14:13:25 +0100

    Initial commit

--- app/Demo/Controller/DemoController.php
+++ app/Demo/Controller/DemoController.php
@@ -2,14 +2,10 @@
 
 namespace App\Demo\Controller;
 
-use App\Models\Client;
 use App\Auth\Models\User;
 use Framework\Router\Annotation\Route;
 use Framework\Renderer\RendererInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Framework\Validator\Validation\ValidationRules;
-use Framework\Invoker\Annotation\ParameterConverter;
-use Framework\Database\ActiveRecord\ActiveRecordQuery;
 
 class DemoController
 {
@@ -34,44 +30,11 @@ class DemoController
         RendererInterface $renderer,
         \PDO $pdo
     ): string {
-        $validation = new ValidationRules('auteur', 'required|min:3|max:10|filter:trim');
-        $validation->isValid('Willy ');
-        $query = new ActiveRecordQuery();
-        $query
-            ->where('id = ?', 'user_id = ?')
-            ->orWhere('created_at = now()')
-            ->setWhereValue([2, 5, new \DateTime()]);
         /** @var \App\Auth\Models\User $user */
         $user = User::find_by_username(['username' => 'admin']);
         $user_array = $user->to_array();
         $mysql_ver = $pdo->getAttribute(\PDO::ATTR_SERVER_VERSION);
         $params = array_merge($request->getServerParams(), $user_array, [$mysql_ver], [$query->__toString()]);
         return $renderer->render('@demo/index', compact('params'));
-    }
-
-    /**
-     * @Route("/react", name="demo.react", methods={"GET"})
-     *
-     * @param RendererInterface $renderer
-     * @return string
-     */
-    public function demoReact(RendererInterface $renderer): string
-    {
-        return $renderer->render('@demo/react');
-    }
-
-    /**
-     * @Route("/demo/client/{id:\d+}", name="demo.client", methods={"GET"})
-     *
-     * @ParameterConverter("client", options={"id"="id", "include"="adresses"})
-     * 
-     * @param \App\Models\Client $client
-     * @param \Framework\Renderer\RendererInterface $renderer
-     * @return string
-     */
-    public function demoClient(Client $client, RendererInterface $renderer): string
-    {
-        $client = $client->to_array(['include' => 'adresses']);
-        return $renderer->render('@demo/client', compact('client'));
     }
 }
