bdeb23018ec88bd9bf93286b0561cdc3325dbb56
William Lety <william.lety@gmail.com>
2021-02-22 13:24:06 +0100

    Initial commit

--- assets/components/TrSelectable.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TrSelectable(props) {
-  return (
-    <tr className={props.isActive ? 'active' : ''}>
-      {props.children}
-    </tr>
-  );
-}
